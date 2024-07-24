2da429718e3240f7fae31358f5d6cf891e1599f5
ys-grouse <malsawma7777@gmail.com>
2024-07-24 12:19:58 +0530

    chore[init]: quasar composables

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
 </template>
 
 <script setup>
+import { useQuasar } from "quasar";
+import { onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+onMounted(() => {
+  window.$q = useQuasar();
+  window.$route = useRoute();
+  window.$router = useRouter();
+})
 defineOptions({
   name: 'App'
 });
