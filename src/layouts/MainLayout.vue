e8a53fcd844e71a85d57f697f5f59475c295a384
blalmal10a <malsawma7777@gmail.com>
2025-01-17 20:02:27 +0530

    feat: update dark mode toggle

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,34 +2,48 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="q-py-xs">
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <div class="row full-width items-center q-col-gutter-sm">
+          <div class="col-auto">
+            <q-btn
+              flat
+              dense
+              round
+              icon="menu"
+              aria-label="Menu"
+              @click="toggleLeftDrawer"
+            />
+          </div>
+          <div class="col">
+            <q-input
+              @update:model-value="(val) => menu.filteredMenuItemList = menu.filterMenuItems(menu.list, val)"
+              debounce="500"
+              v-if="$route.name == 'home' || $route.name == 'edit-order'"
+              dense
+              borderless
+              class="q-px-xs q-ml-sm "
+              style="border: 1px solid white; border-radius: 4px; min-width: 200px;"
+              color="white"
+              :class="{
+                'full-width': $q.screen.lt.sm,
+              }"
+              v-model="menu.filter"
+              type="text"
+              label="Search"
+            />
+          </div>
+          <div>
+            <q-btn
+              outline
+              round
+              :icon="$q.dark.isActive ? 'brightness_2' : 'light_mode'"
+              @click="toggleTheme"
+              size="sm"
+            ></q-btn>
+          </div>
+
+        </div>
 
 
-        <q-input
-          @update:model-value="(val) => menu.filteredMenuItemList = menu.filterMenuItems(menu.list, val)"
-          debounce="500"
-          v-if="$route.name == 'home' || $route.name == 'edit-order'"
-          dense
-          borderless
-          class="q-px-xs q-ml-sm "
-          style="border: 1px solid white; border-radius: 4px; min-width: 200px;"
-          color="white"
-          :class="{
-            'full-width': $q.screen.lt.sm,
-          }"
-          v-model="menu.filter"
-          type="text"
-          label="Search"
-        />
-
-        <!-- <div>Quasar v{{ $q.version }}</div> -->
       </q-toolbar>
     </q-header>
 
@@ -90,14 +104,21 @@
 
 <script setup>
 import { serverUrl } from 'src/boot/main';
-import { diningTable, menu } from 'src/pages/main';
+import { diningTable, main, menu } from 'src/pages/main';
 import { onMounted, ref } from 'vue'
 
 
 onMounted(() => {
   menu.getMenuItems();
   diningTable.getDiningTables();
-  $q.dark.set(true)
+  const lightTheme = localStorage.getItem('light-theme')
+  if (!lightTheme) {
+    $q.dark.set(true);
+  }
+  const listView = localStorage.getItem('item-layout')
+  if (listView) {
+    main.grid = false;
+  }
 })
 
 const leftDrawerOpen = ref(false)
@@ -105,4 +126,18 @@ const leftDrawerOpen = ref(false)
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
+
+function toggleTheme() {
+
+  const dark = $q.dark.isActive
+  if (dark) {
+    //set to light
+    localStorage.setItem('light-theme', true);
+  } else {
+    localStorage.removeItem('light-theme')
+  }
+  $q.dark.toggle();
+}
+
+
 </script>
